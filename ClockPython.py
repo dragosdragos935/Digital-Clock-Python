76e16308ea7b80e422c98906faab024075b33745
Matei Dragos <53956660+dragosdragos935@users.noreply.github.com>
2024-08-04 13:17:03 +0300

    Digital Clock Python v1.0

---
+++ ClockPython.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+
+root = Tk()
+root.title("Clock")
+
+
+def time():
+    string = strftime("%H:%M:%S %p")
+    label.config(text=string)
+    label.after(1000,time)
+
+
+label = Label(root,font=("E1234",80),background = "black",foreground="cyan")
+label.pack(anchor="center")
+time()
+
+root.mainloop()
